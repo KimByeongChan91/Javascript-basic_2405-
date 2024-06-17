badae6e67fe6e1632a008ae7c6f45595da4e0049
KimByeongChan91 <161274451+KimByeongChan91@users.noreply.github.com>
2024-06-17 13:18:52 +0900

    Add files via upload

---
+++ reactTest/util/apple.ts
@@ -0,0 +1,11 @@
+export const appleDivStyle = {
+  color: 'white',
+  backgroundColor: '#000000',
+  width: '200px',
+  height: '30px',
+  borderRadius: '10px',
+  textalign: 'center',
+  gap: '10px',
+};
+
+// const appleElement = <div style={appleDivStyle}>Hello, world!</div>;
